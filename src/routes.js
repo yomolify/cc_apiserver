52784c4e325d9507f315ab51e8545836d7eb1482
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2015-12-18 12:34:44 +0530

    prac get endpoint

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 import Router from 'express'
 import {auth} from './controllers/authController'
 
+var practitioners = require('./controllers/practitionerController')
+
+
 const apiV1 = new Router
 apiV1.use('/v1/auth', auth)
+apiV1.use('/v1/practitioners', practitioners)
 
 export {apiV1}
